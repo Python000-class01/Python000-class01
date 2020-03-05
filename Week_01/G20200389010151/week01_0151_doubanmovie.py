a73cce8e674baf9b360dce358e1cbac4d0af1421
liangchaob <liangchaob@163.com>
2020-03-05 13:48:47 +0800

    rebuild to pep8 style

--- Week_01/G20200389010151/week01_0151_doubanmovie.py
+++ Week_01/G20200389010151/week01_0151_doubanmovie.py
@@ -5,12 +5,13 @@ import requests
 import tqdm
 import csv
 from lxml import etree
+from fake_useragent import UserAgent
 
 CSV_FILE = '豆瓣电影500.csv'
 
 headers = {
     'cache-control': 'no-cache',
-    'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
+    'User-Agent': UserAgent().random,
     'Connection': 'close'
     }
 
@@ -18,29 +19,30 @@ top500_urls = tuple(f'https://movie.douban.com/top250?start={ page * 25}' for pa
 
 csv_header = ['电影名', '评分', '短评数量', '热评1', '热评2', '热评3', '热评4', '热评5']
 
+
 # 主函数
 def main():
     print('获取全部电影链接...')
     # 全部电影链接
-    movie_urls = getAllMovies(top500_urls)
+    movie_urls = get_all_movies(top500_urls)
     # 写入 csv 表头
-    writeCsv(CSV_FILE, csv_header, 'w')
+    write_csv(CSV_FILE, csv_header, 'w')
     print('获取Top500电影信息...')
     # 分别获取每部电影的 url, 并写入 csv
     for movie_url in tqdm.tqdm(movie_urls):
-        movie = getCurrentMovieInfo(movie_url)
+        movie = get_current_movie_info(movie_url)
         # 将电影清洗并整理格式
-        movie_info = dataClean(movie)
+        movie_info = data_clean(movie)
         # 写入 csv
-        writeCsv(CSV_FILE, movie_info, 'a')
+        write_csv(CSV_FILE, movie_info, 'a')
         time.sleep(1)
 
 
 # 获得每部电影的 url
-def getAllMovies(top500_urls):
+def get_all_movies(top500_urls):
     all_movie_urls = []
     for single_page_url in tqdm.tqdm(top500_urls):
-        response = requests.get(single_page_url, headers = headers)
+        response = requests.get(single_page_url, headers=headers)
         e_html = etree.HTML(response.text)
         movie_links = e_html.xpath('//div[@class="pic"]/a/@href')
         all_movie_urls.extend(movie_links)
@@ -50,23 +52,24 @@ def getAllMovies(top500_urls):
 
 
 # 获取指定电影信息
-def getCurrentMovieInfo(movie_url):
-    response = requests.get(movie_url, headers = headers)
+def get_current_movie_info(movie_url):
+    response = requests.get(movie_url, headers=headers)
     e_html = etree.HTML(response.text)
-    movie_name = e_html.xpath('//*[@id="content"]/h1/span[1]/text()')
-    movie_score = e_html.xpath('//*[@id="interest_sectl"]/div[1]/div[2]/strong/text()')
-    movie_comments_count = e_html.xpath('//*[@id="comments-section"]/div[1]/h2/span/a/text()')
-    movie_comments = e_html.xpath('//*[@id="hot-comments"]/div/div/p/span[@class="short"]/text()')
+    movie_name = e_html.xpath('//div[@id="content"]/h1/span[1]/text()')
+    movie_score = e_html.xpath('//strong[@class="ll rating_num"]/text()')
+    comments_count = e_html.xpath('//div[@id="comments-section"]/div[1]/h2/span/a/text()')
+    comments = e_html.xpath('//div[@id="hot-comments"]/div/div/p/span[@class="short"]/text()')
     movie = {
         'name': movie_name[0],
         'score': movie_score[0],
-        'comments_count': movie_comments_count[0],
-        'comments': movie_comments
+        'comments_count': comments_count[0],
+        'comments': comments
     }
     return movie
 
+
 # 数据清洗
-def dataClean(movie):
+def data_clean(movie):
     # 把具体的短评数提取出来
     comments_num = re.findall('\d+', movie['comments_count'])
     # 将电影信息拼成行
@@ -74,10 +77,11 @@ def dataClean(movie):
     movie_info.extend(movie['comments'])
     return movie_info
 
+
 # 将电影信息导入 csv
-def writeCsv(file, line, write_type):
-    out = open(file, write_type, encoding = 'utf-8')
-    csv_write = csv.writer(out, dialect = 'excel')
+def write_csv(file, line, write_type):
+    out = open(file, write_type, encoding='utf-8')
+    csv_write = csv.writer(out, dialect='excel')
     csv_write.writerow(line)
 
 
