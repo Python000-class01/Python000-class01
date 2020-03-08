0aa8ce4444376b3b4531405ab66660f526ba9d4c
bingokorean <lixiaoming728@163.com>
2020-03-08 17:11:12 +0800

    Week_01/G20200389010023/week01_0023_doubanmovie.py

--- Week_01/G20200389010023/week01_0023_doubanmovie.py
+++ Week_01/G20200389010023/week01_0023_doubanmovie.py
@@ -1,7 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 import time
-import pandas as pd
+import csv
 
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.122 Safari/537.36'}
 
@@ -29,22 +29,23 @@ def get_detailcontents(url):
     contents_list.append([film_name,film_score,len_comments,hot_comments])
     return contents_list
 
-def write_to_csv(content_list):
+def write_to_csv(content_list,f):
     columns = ['film_name','film_score','len_comments','hot_comments']
     data = content_list
-    df = pd.DataFrame(columns = columns,data = data)
-    df.to_csv('film_data.csv',encoding='utf-8')
+    writer = csv.writer(f)
+    writer.writerow(columns)
+    writer.writerows(data)
 
 def main():
     base_url = "https://movie.douban.com/top250?start={}&filter="
-    for i in range(0,226,25):
-        url = base_url.format(i)
-        detail_urls = get_detailulrs(url)
-        for detail_url in detail_urls:
-            contents_list = get_detailcontents(detail_url)
-            time.sleep(5)
-            write_to_csv(contents_list)
+    with open("movie_top250.csv","w",encoding="utf-8") as f:
+        for i in range(0,226,25):
+            url = base_url.format(i)
+            detail_urls = get_detailulrs(url)
+            for detail_url in detail_urls:
+                contents_list = get_detailcontents(detail_url)
+                time.sleep(5)
+                write_to_csv(contents_list,f)
 
 if __name__ == "__main__":
     main()
-
