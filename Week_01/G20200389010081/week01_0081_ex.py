dcafeb3d0fd30ce4a1dcd601512efca346ce45bd
buyinta <buyinta2012@163.com>
2020-03-11 00:08:41 +0800

    豆瓣数据爬取

--- Week_01/G20200389010081/week01_0081_ex.py
+++ Week_01/G20200389010081/week01_0081_ex.py
@@ -1,42 +1,68 @@
+import time
+import csv
 import requests
-from bs4 import BeautifulSoup as bs
-
+from pprint import pprint
+from lxml import html
+etree = html.etree
 
 def get_url_name(myurl):
     user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36'
     header = {}
     header['user-agent'] = user_agent
     response = requests.get(myurl, headers=header)
-    bs_info = bs(response.text, 'html.parser')
-    total_list=[]
-    for tags in bs_info.find_all('div', attrs={'class': 'info'}):
-        movie_list=[]
-        for tag in tags.find_all('div', attrs={'class': 'hd'}):
-            for title_tag in tag.find_all('span', class_='title'):
-                # print(title_tag.get_text())
-                movie_list.append(title_tag.get_text())
-        for tag in tags.find_all('div', attrs={'class': 'bd'}):
-            for star_tag in tag.find_all('span', class_='rating_num'):
-                # print(star_tag.get_text())
-                movie_list.append(star_tag.get_text())
-            for comment_tag in tag.find_all('span',class_=''):
-                # print(comment_tag.get_text())
-                movie_list.append(comment_tag.get_text())
-        total_list.append(movie_list)
-    # with open('movie.csv','a') as f:
-    #     for list1 in total_list:
-    #         for list2 in list1:
-    #             f.write(list2)
 
-    print(total_list)
+class DouBanSpider():
+    def __init__(self,max_page=5):
+        self.base_url='https://movie.douban.com/top250?start={}&filter='
+        self.headers={
+            'user-agent':'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36'
+            }
+        self.max_page=max_page
+        self.list=[]
 
-urls = tuple(f'https://movie.douban.com/top250?start={page * 25}&filter=' for page in range(3))
+    def get_url_list(self):
+        url_list=[]
+        for page in range(0,self.max_page,25):
+            url=self.base_url.format(page)
+            url_list.append(url)
 
-from time import sleep
+        return url_list
+
+    def get_content(self,url):
+
+        response=requests.get(url=url,
+                              headers=self.headers)
+
+        return response.content
+
+    def get_items(self,content):
+        text=content.decode('utf-8')
+        htmlDiv = etree.HTML(text)
+        target_list=htmlDiv.xpath('//div[@class="info"]')
+        for target in target_list:
+            # print(target)
+            item={}
+            item['titele']=target.xpath('//div[@class="hd"]/a/span[@classs="title"]/text()')
+            item['rating_num']=target.xpath('span[@class="rating_num"]/text()')
+            self.list.append(item)
+        return self.list
+
+    def save_items(self,items):
+        with open('./0081.csv','w',encoding='utf-8') as f:
+            csv_writer=csv.writer(f)
+            for item in items:
+                csv_writer.writerow(item.values())
+
+    def main(self):
+        # 1. 获取 url 列表
+        url_list = self.get_url_list()
+
+        for url in url_list:
+            time.sleep(1)
+            content = self.get_content(url)
+            items=self.get_items(content)
+            self.save_items(items)
 
 if __name__ == '__main__':
-    for page in urls:
-        get_url_name(page)
-        sleep(5)
-
-######################
+    spider=DouBanSpider(max_page=1)
+    spider.main()
