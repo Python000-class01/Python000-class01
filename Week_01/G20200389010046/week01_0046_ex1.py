5e01c817463cd241154757d067b3c29e1a540985
liangfei <liangfei90@163.com>
2020-03-11 17:31:51 +0800

    1.submit 0046 homework

--- Week_01/G20200389010046/week01_0046_ex1.py
+++ Week_01/G20200389010046/week01_0046_ex1.py
@@ -19,65 +19,54 @@ def parsebooklist(htmltext):
     titles = book_info.xpath('//div[@class="pl2"]/a/@title')
     refs = book_info.xpath('//div[@class="pl2"]/a/@href')
     scores = book_info.xpath('//div[@class="star clearfix"]/span[@class="rating_nums"]/text()')
-    
     booklist = [{'title':title,'ref':ref,'score':score} for title,ref,score in  zip(titles,refs,scores)]
-
     return booklist
 
-def parsebookdetails(html,bookbasic):
+def parsebookdetails(bookbasicinfo):
 
-    bookfull = bookbasic
-
-    htmltext = getcontent(html)
+    htmltext = getcontent(bookbasicinfo['ref'])
     book_detail = etree.HTML(htmltext)
 
     # xpath solution
+    # wtm 豆瓣的前端写的实在是有点烂 
 
-    auther = book_detail.xpath('//div[@id="info"]/span[1]/a/text()')
+    auther = book_detail.xpath('//div[@id="info"]/span/span[text()=" 作者"]/following-sibling::*[1]/text()')
+    # price = book_detail.xpath('//div[@id="info"]/span[text()="定价:"]/following-sibling::*[1]/text()')
     price = book_detail.xpath('//div[@id="info"]/text()[9]')
     comment = book_detail.xpath('//p[@class="comment-content"]/span[@class="short"]/text()')
 
 
-    # booklist = [{'title':title,'ref':ref,'score':score} for title,ref,score in  zip(titles,refs,scores)]
+    bookfullinfo = bookbasicinfo
 
     try:
-        bookfull['auther'] = auther[0]
-        bookfull['price'] = price[0]
-        bookfull['comment'] = comment[0:5]
+        bookfullinfo['auther'] = auther[0]
+        bookfullinfo['price'] = price[0]
+        bookfullinfo['comment'] = comment[0:5]
     except:
         pass
 
-    return bookfull
+    return bookfullinfo
  
 
 
 if __name__ == '__main__':
 
-    booknewlist =[]
-
+    # get top 250 booklist
+    booklist =[]
     for i in range(1):
         url = 'https://book.douban.com/top250?start='+str(i*25)
         mybookinfo = getcontent(url)
-        booknewlist.extend(parsebooklist(mybookinfo))
+        booklist.extend(parsebooklist(mybookinfo))
         sleep(0.5)
-    pdbook = pd.DataFrame(booknewlist)
 
-
-    bookdetail = []
-    for item in booknewlist:
-        oneurl = item['ref']
-        print(item)
-        onedetail = parsebookdetails(oneurl,item)
-        bookdetail.append(onedetail)
+    # get book details
+    bookdetaillist = []
+    for item in booklist:
+        itemdetail = parsebookdetails(item)
+        bookdetaillist.append(itemdetail)
         sleep(0.5)
-        print('currnet book is %s' % item['title'])
-    pddetail = pd.DataFrame(bookdetail)
+        print('currnet book is 《%s》' % item['title'])
+    result = pd.DataFrame(bookdetaillist)
 
-    pddetail.to_csv('22.csv',encoding='utf-8')
-
-
-
-
-
-# a = tuple(f'https://book.douban.com/top250?start={page*25}' for page in range(3))
-
+    # save to csv
+    result.to_csv('result.csv',encoding='utf-8')
