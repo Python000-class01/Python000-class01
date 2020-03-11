aae77fde70f90e63da2a57ad8c229a8750fc0878
李育倩 <liyuqian@riikus-MacBook-puro.local>
2020-03-11 14:22:13 -0700

    hw1 -v2

--- Week_01/G20200343030408/hw1.py
+++ Week_01/G20200343030408/hw1.py
@@ -3,7 +3,8 @@ from bs4 import BeautifulSoup as bs
 import csv
 
 # hw1 安装并使用 requests、bs4 库，爬取豆瓣电影 Top250 的电影名称、评分、短评数量和前 5 条热门短评，并以 UTF-8 字符集保存到 csv 格式的文件中。
-collection=[]
+collection = []
+
 def get_url_name(url):
     # url = 'https://movie.douban.com/top250'
     user_agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36'
@@ -19,25 +20,27 @@ def get_url_name(url):
     #     print(atags.get('title'))
     # print(response.text)
     for tags in bs_info.find_all('div', attrs={'class': 'info'}):
+        comments_list = []
         titles = tags.find_all('span', attrs={'class': 'title'})
         rates = tags.find_all('span', attrs={'class': 'rating_num'})
         atags = tags.find_all('div', attrs={'class': 'hd'})
         #print(titles[0])
-        collection.append([titles[0].text])
+        #collection.append([titles[0].text])
         #print(rates[0])
-        collection.append([rates[0].text])
-        #print(atags[0])
-        collection.append([atags[0].text])
+        #collection.append([rates[0].text])
         for hrefs in tags.find_all('a'):
             comments_page = (hrefs.get('href'))
             comment_response = requests.get(comments_page,headers=header)
             comment_url = bs(comment_response.text, 'html.parser')
             comments = comment_url.find_all('div', attrs={'class':'comment'})
-            #print(len(comments))
-            for i in range(4):
+            num_url = comment_url.find_all('div', attrs={'class':'mod-hd'})
+            num = num_url[0].find_all('span', attrs={'class':'pl'})
+            comment_numbers=num[0].find_all('a')[0].text
+            for i in range(0,5):
                 short_comment = comments[i].find_all('span', attrs={'class':'short'})
                 #print(short_comment)
-                collection.append([short_comment.text])
+                comments_list.append(short_comment[0].text)
+        collection.append([titles[0].text,rates[0].text,comment_numbers,comments_list])
 
 
 
@@ -70,13 +73,13 @@ if __name__ == '__main__':
     f = open('movies.csv', 'w', newline='', encoding='utf-8')
     writer = csv.writer(f)
     writer.writerow(['movie_name', 'rating','comment nums', 'hot comments'])
-    for page in range(1):
+    for page in range(10):
         astring = 'https://book.douban.com/top250?start={ page * 25}'
         urls = tuple(astring)
         url = 'https://movie.douban.com/top250?start='+str(page*25)
         #print(url)
         get_url_name(url)
-        sleep(5)
+        sleep(10)
     for row in collection:
         print(row)
         writer.writerow(row)
