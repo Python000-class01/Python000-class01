175e4079d45f7f509fefc393f6bccb541c55b402
siloen <893557024@qq.com>
2020-03-11 16:32:01 +0800

    exercise week01

--- Week_01/G20190389010007/week01_0007_ex_02.py
+++ Week_01/G20190389010007/week01_0007_ex_02.py
@@ -6,15 +6,6 @@ get_params= {'name':'Jack','age':30}
 post_params= {'name':'Rose','age':31}
 reponse_get = requests.get(baseurl+"get",params=get_params)
 reponse_post = requests.post(baseurl+"post",params=post_params)
+josn_get = reponse_get.json()
+josn_post = reponse_post.json()
 
-#to dict
-josn_get = json.loads(reponse_get.text)
-
-#to dict
-josn_post = json.loads(reponse_post.text)
-print(reponse_get)
-print(type(reponse_get))
-print(josn_get)
-print(reponse_post)
-print(type(reponse_get))
-print(josn_post)
