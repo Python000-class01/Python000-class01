b02e7114a4b950007846e71e3b096fba779436d6
yuanjiaye <2508182476@qq.com>
2020-03-12 16:49:50 +0800

    学习笔记 G20200389010086-week02

---
+++ Week_02/G20200389010086/week_learn.py
@@ -0,0 +1,11 @@
+import math
+
+
+def enroll(name, gender, age=6, city='Beijing'):
+    print('name:', name)
+    print('gender:', gender)
+    print('age:', age)
+    print('city:', city)
+
+
+print(enroll('zhansan', 'f', city='hah'))
