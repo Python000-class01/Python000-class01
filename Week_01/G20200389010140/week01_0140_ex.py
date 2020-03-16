f5add83d4a25a2fc439e22aa2bc8e0753fdabaec
Cor <cor@MiWiFi-R3-srv.nosure>
2020-03-16 23:41:37 +0800

    add write to csv

--- Week_01/G20200389010140/week01_0140_ex.py
+++ Week_01/G20200389010140/week01_0140_ex.py
@@ -3,6 +3,11 @@ import csv
 from time import sleep
 from bs4 import BeautifulSoup as bs
 
+def record_in_csv(datas):
+    with open('result2.csv','a+') as file:
+        csv_file = csv.writer(file)
+        csv_file.writerow(datas)
+
 # Python 使用def定义函数，myurl是函数的参数
 def get_url_name(myurl):
    user_agent = 'Mozilla/5.0 (X11; Linux x86_64; rv:68.0) Gecko/20100101 Firefox/68.0'
@@ -12,11 +17,13 @@ def get_url_name(myurl):
    bs_info = bs(response.text, 'html.parser')
 
    # Python 中使用 for in 形式的循环,Python使用缩进来做语句块分隔
-   
    for tags in bs_info.find_all('div', attrs={'class': 'pl2'}):
-       #print(tags)
-       for atag in tags.find_all('span',):
-          print(atag)            
+       for atag in tags.find_all('a',):
+           # 获取所有链接
+           data = (atag.get('href'))+(atag.get('title'))
+           print(data)
+           record_in_csv(data)
+
 
 # 生成包含所有页面的元组
 urls = tuple('https://book.douban.com/top250?start=%d'%page for page in range(0,100,25))
