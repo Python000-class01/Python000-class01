8224309f440373787d18fae4c002e4af2924f15d
swortect <swortect@gmail.com>
2020-03-17 09:19:15 +0800

    作业

--- Week_02/G20200389010098/NOTE.md
+++ Week_02/G20200389010098/NOTE.md
@@ -1,1 +1,77 @@
-学习笔记
+带参数的装饰器多包了一层  
+wrapt完善的装饰器  
+functools.lru_cache可以缓存函数结果，在运算复杂的递归函数中特别有用  
+functools.lru_cache(maxsize=128, typed=False)有两个可选参数  
+maxsize代表缓存的内存占用值，超过这个值之后，就的结果就会被释放  
+typed若为True，则会把不同的参数类型得到的结果分开保存  
+可以用类去装饰函数  
+装饰类一定要用类  
+向一个函数添加属性  
+```
+def attrs(**kwds):
+    def decorate(f):
+        for k in kwds:
+            setattr(f, k, kwds[k])
+        return f
+    return decorate
+@attrs(versionadded="2.2",
+       author="Guido van Rossum")
+def mymethod(f):
+    pass
+```
+dataclass可以简化类的普通字段的定义  
+静态变量：类中定义不带self的变量，可在类以外定义 
+静态变量每个实例都有一份，相互不干扰  
+内置类型不能直接增加属性和方法  
+私有变量用 __定义 会自动改名成_Human2__fly，但其实还是很访问  
+\__getattribute__返回存在的属性，如果不存在抛出  AttributeError 异常，继续访问getattr__函数  
+如果同时存在，执行顺序是getattribute__>getattr__>dict__  
+python中，一个类实现了\__get__, \__set__，\__delete__的三个方法中的任意一个就是描述器  
+1.如果仅仅实现了\__get__.就是非数据描述器 non-data descriptor  
+2.同时实现了\__get__,\__set__,就是数据描述器，data descriptor  
+如果一个类的类属性，设置为描述器，那么这个类被称为owner属主，method也是类的属性  
+用描述器可以对传入值的类型范围做限制  
+property 可以将复杂的逻辑作为属性返回，property的setter和deleter的方法名最好相同  
+property经典场景可以用来判断状态，和偏函数结合可以做公有云分发
+property的方法改写成 \_Article__gender  
+@classmethod 可以将类的方法静态化，可以不实例话就直接调用，但是self要换成cls  
+@staticmethod 不传入self 也不传入 cls，所以不能使用类属性和实例属性。一般作为和类有关系的独立函数使用，保持统一管理  
+werkzeug.utils 的 cached_property 可以实现类方法的缓存，flask里也多有用到  
+子类要继承父类的属性要在\__init__里super  
+super().__init__(name)  
+super(Man, self).__init__()  
+type元类由type自身创建，object类由元类type创建  
+type类继承了object类  
+所有都是由type创建  
+所有都继承自object  
+多继承，可以用.mro()查看继承顺序，左侧追述到和右侧同一个父为止  
+没有多态，同名函数会被覆盖  
+1.\__init__ 通常用于初始化一个新实例，控制这个初始化的过程，比如添加一些属性， 做一些额外的操作，发生在类实例被创建完以后。它是实例级别的方法。  
+2.\__new__ 通常用于控制生成一个新实例的过程。它是类级别的方法。  
+https://www.cnblogs.com/shenxiaolin/p/9307496.html  
+工厂模式可以在函数内动态创建的类  
+```
+def factory2(func):
+    class klass: pass
+    #setattr需要三个参数:对象、key、value
+    setattr(klass, func.__name__, func )
+    return klass
+def say_foo(self): print('bar')
+Foo = factory2(say_foo)
+foo = Foo()
+foo.say_foo()
+```
+使用type 元类创建类  
+```
+def hi():
+    print('Hi metaclass')
+Foo = type('Foo',(),{'say_hi':hi})
+foo = Foo
+foo.say_hi()
+```
+Mixin可以理解为python的接口  
+```
+MySubClass(LoggerMixin,Displayer)
+```
+Displayer定义接口，Mixin具体实现  
+metaclass可以扩展内置数据类型，赋予新的方法 
---
+++ Week_02/G20200389010098/week02_0098_mall.py
@@ -0,0 +1,95 @@
+import pandas as pd
+import numpy as np
+#模拟数据库存储用户商品信息
+user = [{"uid": 1, "user_name": "customer_1", "is_vip": 0},
+        {"uid": 2, "user_name": "customer_vip", "is_vip": 1}]
+goods = [{"gid": 1, "goods_name": "coffee", "price": 25.25},
+         {"gid": 2, "goods_name": "water", "price": 10.11},
+         {"gid": 3, "goods_name": "bread", "price": 30.55},
+         {"gid": 4, "goods_name": "desk", "price": 55.22}]
+class MyMall:
+    def __init__(self, uid):
+        self.user_info = self.findUser(uid)
+        # 用户不存在报错
+        if(self.user_info == False):
+            print("对不起，用户不存在")
+        self._cart = []
+
+    # 模拟查询用户
+    def findUser(self, uid):
+        rs = [x for x in user if (x['uid'] == uid)]
+        if len(rs) > 0:
+            return rs[0]
+        else:
+            return False
+
+    # 属性返回是否vip
+    @property
+    def is_vip(self):
+        if (self.user_info == False):
+            return False
+        else:
+            if (self.user_info['is_vip'] == 0):
+                return False
+            else:
+                return True
+
+    # 模拟查询商品
+    def findGoods(self, gid):
+        rs = [x for x in goods if (x['gid'] == gid)]
+        if len(rs) > 0:
+            return rs[0]
+        else:
+            return False
+    # 装饰器，处理优惠信息，pandas展示更直观
+    def discount(func):
+        def inner(self, *args, **kwargs):
+            ret = func(self, *args, **kwargs)
+            discount_fee = 0
+            count = np.sum([x['num'] for x in ret])
+            total = np.sum([x['num'] * self.findGoods(x['gid'])['price'] for x in ret])
+            if not self.is_vip:
+                if total >= 200:
+                    discount_fee = np.around(total - np.around(total * 0.9, 2))
+            else:
+                if total >= 200:
+                    discount_fee = np.around(total - np.around(total * 0.8, 2))
+                if count >= 10:
+                    temp = np.around(total - np.around(total * 0.85, 2))
+                    if temp > discount_fee:
+                        discount_fee = temp
+            names = [goods[x['gid']]['goods_name'] for x in ret]
+            info = [[goods[x['gid']]['price'], x['num'], np.around(x['num'] * goods[x['gid']]['price'], 2)] for x in
+                    ret]
+            names.append('-' * 10)
+            info.append(['-' * 10, '-' * 10, '-' * 10])
+            names.append("discount")
+            info.append(['', '', -1*discount_fee])
+            names.append("sum")
+            info.append(['', count, total])
+            df = pd.DataFrame(info, index=names, columns=['price', 'num', 'sum'])  # 生成6行4列位置
+            return df
+        return inner
+
+    # 获取购物车，用装饰器生成优惠
+    @property
+    @discount
+    def cart(self):
+        return self._cart
+
+    # set购物车的值，这里只做property的复习，实际不需要
+    @cart.setter
+    def addCart(self, info):
+        gid = 0
+        num = 1
+        if(isinstance(info, int)):
+            gid = info
+        elif (isinstance(info, list)):
+            gid = info[0]
+            num = info[1]
+        self._cart.append({"gid": gid, "num": num})
+
+mall = MyMall(2)
+mall.addCart = 1
+mall.addCart = [2, 20]
+print(mall.cart)
