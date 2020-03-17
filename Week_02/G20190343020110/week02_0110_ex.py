b38bcf9dfca10d2c79c813e586f1bf11da737ef2
黄雄 <hxdream_sz@126.com>
2020-03-17 19:38:28 +0800

    类名字修改

--- Week_02/G20190343020110/week02_0110_ex.py
+++ Week_02/G20190343020110/week02_0110_ex.py
@@ -5,21 +5,22 @@ from decimal import *
 # 设置全局精度
 getcontext().prec = 6
 
-class good_info(object):
-    good_name = ''
-    good_price = 0
+class product_info(object):
+    product_name = ''
+    product_price = 0
     # def __init__(self,good_name,good_price):
     #     self.good_name
     #     self.good_price
         
-class payGood(object):
+
+class payProduct(object):
     money = 0.00
     is_vip  = False
-    good_list = []
+    product_list = []
 
-    def __init__(self, is_vip, good_list):
+    def __init__(self, is_vip, product_list):
         self.is_vip = is_vip
-        self.good_list = good_list
+        self.product_list = product_list
         self._cost = 0
         
     @property
@@ -28,44 +29,44 @@ class payGood(object):
     
     @cost.getter
     def cost(self):
-        if self.good_list == None:
+        if self.product_list == None:
             print('good_list is none')
             return Decimal(0).quantize(Decimal('0.00'))
         #算出商品总费用
-        good_sum_price = self.sumGoodPrice()
+        product_sum_price = self.sum_product_price()
         if self.is_vip:
             #计算商品没打扰时，总费用
-            good_sum_price2 = good_sum_price
+            product_sum_price2 = product_sum_price
             #判断总费用大于200时的花费
-            if good_sum_price >= 200:
-                good_sum_price = good_sum_price * 0.8
+            if product_sum_price >= 200:
+                product_sum_price = product_sum_price * 0.8
             ##计算商品数量大于10个时的花费
-            if len(self.good_list) > 10:
-                good_sum_price2 * 0.85
+            if len(self.product_list) > 10:
+                product_sum_price2 * 0.85
             #使用三目表达式，算出最优惠的费用
-            return Decimal(good_sum_price if(good_sum_price2 > good_sum_price) else good_sum_price2).quantize(Decimal('0.00'))
+            return Decimal(product_sum_price if(product_sum_price2 > product_sum_price) else product_sum_price2).quantize(Decimal('0.00'))
         else:
             #如果是普通用户的，费用计算方式
-            if good_sum_price >= 200:
-                return Decimal(good_sum_price * 0.9).quantize(Decimal('0.00'))
-            return Decimal(good_sum_price).quantize(Decimal('0.00'))
+            if product_sum_price >= 200:
+                return Decimal(product_sum_price * 0.9).quantize(Decimal('0.00'))
+            return Decimal(product_sum_price).quantize(Decimal('0.00'))
     
-    def sumGoodPrice(self):
-        if self.good_list == None:
+    def sum_product_price(self):
+        if self.product_list == None:
             return 0
         sum = 0
-        for i in range(len(self.good_list)):
-            sum = sum + self.good_list[i].good_price
+        for i in range(len(self.product_list)):
+            sum = sum + self.product_list[i].product_price
         print(sum)
         return sum
 
 
-good_list = []
+product_list = []
 for i in range(0,20):
     # good = good_info(f'商品 {i}', i)
-    good = good_info()
-    good.good_name = f'商品 {i}'
-    good.good_price = i*10
-    good_list.append(good)
-p = payGood(True, good_list)
+    product = product_info()
+    product.product_name = f'商品 {i}'
+    product.product_price = i*10
+    product_list.append(product)
+p = payProduct(True, product_list)
 print(p.cost)
