7354dfa6385e4f87fa112dd7940feda11f9575ee
zsingsun <45170326+zsingsun@users.noreply.github.com>
2020-03-18 18:04:01 +0800

    Update

--- Week_02/G20200389010176/week02_0176_1.py
+++ Week_02/G20200389010176/week02_0176_1.py
@@ -2,6 +2,28 @@
 # 为“996 便利店”设计一套销售系统的结算模块;
 # 结算模块要求对不同类型的用户（普通用户、VIP 用户）的单次消费进行区分并按照不同的购买金额、不同的用户身份进行结账
 
+class Customer():
+    def __init__(self, name, is_vip):
+        self.name = name
+        self.is_vip = is_vip
+        
+
+class Order():
+    def __init__(self, customer, shopping_cart):
+        self.customer = customer
+        self.shopping_cart = shopping_cart
+    
+    @property
+    def total_price(self):
+        total_price = 0
+        for item in self.shopping_cart:
+            total_price += self.shopping_cart[item]
+        return total_price
+    
+    @property
+    def goods_account(self):
+        return len(self.shopping_cart)
+        
         
 class Cashier():
     
@@ -13,11 +35,11 @@ class Cashier():
     4、VIP会员满10件商品打八五折。
     由于VIP会员存在两种折扣方式，需自动根据最优惠的价格进行结算
     '''
-
-    def __init__(self, total_price, goods_account, is_vip):
-        self.total_price = total_price
-        self.goods_account = goods_account
-        self.is_vip = is_vip
+        
+    def __init__(self, order):
+        self.total_price = order.total_price
+        self.goods_account = order.goods_account
+        self.is_vip = order.customer.is_vip
     
     
     def __normal_checkout(self):
@@ -30,6 +52,7 @@ class Cashier():
         if self.total_price >= 200:
             return self.total_price * 0.8
         elif self.goods_account >= 10:
+#            print('check')
             return self.total_price * 0.85
         else:
             return self.total_price
@@ -41,26 +64,47 @@ class Cashier():
         return self.__normal_checkout()
 
 
-# 普通用户消费不足200元，无折扣，原价付费        
-c1 = Cashier(100, 1, False)
-print(c1.checkout())
+c1 = Customer('Jack', False)
+c2 = Customer('Lucy', True)
 
-# 普通用户消费满200元打九折       
-c2 = Cashier(200, 10, False)
-print(c2.checkout())
+s_price100 = {'good1': 100}
+s_price200 = {'good1': 200}
+s_account10 = {f'good{i}':i for i in range(1,11)}
+s_price200_account10 = {f'good{i}':i*5 for i in range(1,11)}
 
-# VIP会员无折扣，原价付费        
-c3 = Cashier(100, 9, True)
-print(c3.checkout())
 
-# VIP会员满200元打八折       
-c4 = Cashier(200, 1, True)
-print(c4.checkout())
+# 普通用户消费满200元打九折
+o1 = Order(c1, s_price200)
+
+# 普通用户消费不足200元，无折扣，原价付费 
+o2 = Order(c1, s_price100)
+
+# VIP会员满200元打八折 
+o3 = Order(c2, s_price200)
 
 # VIP会员满10件商品打八五折 
-c5 = Cashier(100, 10, True)
-print(c5.checkout())
+o4 = Order(c2, s_account10)
 
 # VIP会员满200元,满10件商品,打八折 
-c6 = Cashier(200, 10, True)
-print(c6.checkout())
+o5 = Order(c2, s_price200_account10) 
+
+# VIP会员无折扣，原价付费 
+o6 = Order(c2, s_price100)
+
+p1 = Cashier(o1)
+print(f'原价：{p1.total_price} 折后应付：{p1.checkout()} 折扣：{10 * p1.checkout() / p1.total_price}')
+
+p2 = Cashier(o2)
+print(f'原价：{p2.total_price} 折后应付：{p2.checkout()} 折扣：{10 * p2.checkout() / p2.total_price}')
+
+p3 = Cashier(o3)
+print(f'原价：{p3.total_price} 折后应付：{p3.checkout()} 折扣：{10 * p3.checkout() / p3.total_price}')
+
+p4 = Cashier(o4)
+print(f'原价：{p4.total_price} 折后应付：{p4.checkout()} 折扣：{10 * p4.checkout() / p4.total_price}')
+
+p5 = Cashier(o5)
+print(f'原价：{p5.total_price} 折后应付：{p5.checkout()} 折扣：{10 * p5.checkout() / p5.total_price}')
+
+p6 = Cashier(o6)
+print(f'原价：{p6.total_price} 折后应付：{p6.checkout()} 折扣：{10 * p6.checkout() / p6.total_price}')
