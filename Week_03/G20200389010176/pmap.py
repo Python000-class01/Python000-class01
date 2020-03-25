1d48120a5131410003263f9b0027ad08eec10b50
zsingsun <45170326+zsingsun@users.noreply.github.com>
2020-03-25 22:45:03 +0800

    scanner

--- Week_03/G20200389010176/pmap.py
+++ Week_03/G20200389010176/pmap.py
@@ -4,32 +4,36 @@ import threading
 import argparse
 
 import subprocess
-from Queue import Queue  
-#import IPy
-import ipaddr
+#from Queue import Queue  
+from netaddr import *
+import json
+
 
 lock = threading.Lock()
 threads = []
 
 
-def ping_check(host):
-    
+def ping_check(host, ping_result):
+    #ping_result = {}
     ret = subprocess.call('ping -c 1 %s' % host, shell=True, stdout=subprocess.PIPE)  
-    
-    if ret == 0:  
-        print('%s is up!' % host)
-
+    if ret == 0:
+        status = 'up'  
+        print(f'{host} is {status}')
     elif ret == 1:  
-        print('%s is down...' % host)  
+        status = 'down'
+        print(f'{host} is {status}')
+    
+    ping_result[host] = status
+    
 
-
-def port_scan(host, port):
+def port_check(host, port, port_result):
     
     try:
         s = socket(AF_INET, SOCK_STREAM)
         s.connect((host, port))
         lock.acquire()
-        print('[+] %d open' % port)
+        print(f'[+] {host}:{port} open')
+        port_result.append(port)
         lock.release()
         s.close()
     except:
@@ -47,41 +51,53 @@ def main():
     args = p.parse_args()
     concurrent_connections = args.concurrent
     check_method = args.method
-    host_list = ipaddr.IPNETWORK(args.hosts)
     output_file = args.output
-
-    setdefaulttimeout(1)
+    
+    host_list = args.hosts.split(',')
 
     port_list = list(range(1, 1024))
    
+    setdefaulttimeout(0.5)
 
     if check_method == 'ping':
+        ping_result = {}
         for host in host_list:
-            print('Pinging the host: %s...' % host)
-
-            t = threading.Thread(target=ping_check, args=(host,))
+            t = threading.Thread(target=ping_check, args=(host, ping_result))
             threads.append(t)
             t.start()
 
             for t in threads:
                 t.join()
 
-            print('[*] The host: %s ping checking is complete!' % host)
+        if output_file.strip() != '':
+            try:
+                with open(output_file, 'w', encoding='utf-8') as fs:
+                    json.dump(ping_result, fs)
+            except IOError as e:
+                print(e)
 
     elif check_method == 'tcp':
-        for host in host_list:
-            print('Scanning the host: %s...' % host)
         
+        tcp_result = {}
+        for host in host_list:
+            port_result = []
             for p in port_list:
-                t = threading.Thread(target=port_scan, args=(host, p))
+                t = threading.Thread(target=port_check, args=(host, p, port_result))
                 threads.append(t)
                 t.start()     
-
+            
             for t in threads:
                 t.join()
+            
+            tcp_result[host] = port_result
 
-            print('[*] The host: %s scan is complete!' % host)
-
+        if output_file.strip() != '':
+            try:
+                with open(output_file, 'w', encoding='utf-8') as fs:
+                    json.dump(tcp_result, fs)
+            except IOError as e:
+                print(e)
+        
 
 if __name__ == '__main__':
     main()
