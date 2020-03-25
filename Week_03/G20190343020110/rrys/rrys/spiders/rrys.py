f1c74dbb7720d5bdc0decf1da6b64ee63d9e1546
黄雄 <hxdream_sz@126.com>
2020-03-25 22:17:34 +0800

    修改抓取游览数和影视等级

--- Week_03/G20190343020110/rrys/rrys/spiders/rrys.py
+++ Week_03/G20190343020110/rrys/rrys/spiders/rrys.py
@@ -2,6 +2,8 @@
 import scrapy
 import lxml.etree
 from rrys.items import RrysItem
+import json
+import re
 
 class RrysSpider(scrapy.Spider):
     name = 'rrys'
@@ -20,9 +22,13 @@ class RrysSpider(scrapy.Spider):
                 f"/html/body/div[2]/div/div[1]/div/ul/li[{i}]/a/@href")
             link = f'http://www.rrys2019.com{url[0]}'
             print(f"link={link}")
+            print(link.split('/')[-1])
+            
             yield scrapy.Request(url=link, meta={'item': item}, callback=self.parse2)
     
     def parse2(self, response):
+        link = response.url
+        resourceId = link.split('/')[-1]
         item = response.meta['item']
         selector = lxml.etree.HTML(response.text)
         score = selector.xpath(
@@ -32,8 +38,20 @@ class RrysSpider(scrapy.Spider):
         imgSrc = selector.xpath(
             "/html/body/div[2]/div/div[1]/div[1]/div[2]/div[2]/div/img/@src")[0]
         print(f'imgSrc={imgSrc}')
-        item['imgSrc'] = imgSrc
-        viewCount = selector.xpath('//*[@id="resource_views"]/text()')
-        print(f'viewCount={viewCount}')
-        item['viewCount'] = viewCount
+        s = re.search(r'/\w+?-big', imgSrc).group(0)
+        print(s[1])
+        item['imgSrc'] = s[1]
+        # viewCount = selector.xpath("//label[@id='resource_views']")
+        # print(f'viewCount={viewCount}')
+        # item['viewCount'] = viewCount
+        print(resourceId)
+        viewLink = f'http://www.rrys2019.com/resource/index_json/rid/{resourceId}/channel/tv'
+        yield scrapy.Request(url=viewLink, meta={'item': item}, callback=self.parse3)
+
+    def parse3(self, response):
+        item = response.meta['item']
+        json_str = response.text[15:]
+        data = json.loads(json_str)
+        print(data['views'])
+        item['viewCount'] = data['views']
         yield item
