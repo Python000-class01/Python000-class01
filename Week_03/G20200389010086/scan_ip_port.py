1f199bf66d680ae37ab9793e3ad1151188ca6af9
yuanjiaye <2508182476@qq.com>
2020-03-25 23:31:02 +0800

    粗略的端口扫描 G20200389010086-03

--- Week_03/G20200389010086/scan_ip_port.py
+++ Week_03/G20200389010086/scan_ip_port.py
@@ -1,14 +1,61 @@
-import time
-from multiprocessing import Process
+
+from multiprocessing.dummy import Pool as ThreadPoll
 import os
+import socket
+import json
 
 
-def run():
-    pass
+# 创建多线程
+def create_threadPoll(n):
+    Poll = ThreadPoll(n)
+    return Poll
+
+
+def ping_ip(ip):
+    back_info = os.system('ping -c 1 -w 1 %s' % ip)
+    return back_info
+
+
+def ping_all_ip(ip, poll):
+    result = poll.map(ping_ip, ip)
+    poll.close()
+    poll.join()
+    with open('result.json', 'a+') as f:
+        json.dump(result, f)
+
+
+def scan_all_port(ip, ports, poll):
+    port = (i for i in range(ports))
+    result = poll.map(socket_connect, args=(ip, port))
+    poll.close()
+    poll.join()
+    if result == 0:
+        with open('port.josn', 'a+') as f:
+            json.dump(port, f)
+
+
+# 创建tcp 连接
+def socket_connect(ip, port):
+    s = socket.socket()
+    s.settimeout(5)
+    try:
+        return s.connect_ex((ip, port))
+    except socket.error as e:
+        return False
 
 
 if __name__ == "__main__":
-    p = Process(target=run)
 
-    p.start()
-    p.join()
+    n = int(input('请输入并发连接数'))
+    t = str(input('请输入类型'))
+    ip = input('请输入ip')
+    poll = create_threadPoll(n)
+    if t == 'ping':
+        ping_all_ip(ip, poll)
+    elif t == 'tcp':
+
+        max_ports = 65535
+        scan_all_port(ip, max_ports, poll)
+    else:
+        print('输入有误，请输入 tcp 或 ping ')
+        exit()
