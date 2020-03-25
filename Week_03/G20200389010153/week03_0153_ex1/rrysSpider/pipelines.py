ecf52097d39712ba4d00836acdb433c6edd05f55
moxiaoduoduo <522262704@qq.com>
2020-03-25 19:33:48 +0800

    G201903430100153-Week 03
    作业提交

---
+++ Week_03/G20200389010153/week03_0153_ex1/rrysSpider/pipelines.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+# Define your item pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
+
+
+from scrapy.exporters import CsvItemExporter
+
+
+class RrysspiderPipeline(object):
+    def open_spider(self, spider):
+        self.file = open('./rrysHotDownloadedMovies.csv', 'wb')
+        self.exporter = CsvItemExporter(self.file,
+                                        fields_to_export=["title", "href_url", "rank", "level", 'viewcount', 'coverinfo'])
+        self.exporter.start_exporting()
+
+    def process_item(self, item, spider):
+        self.exporter.export_item(item)
+        return item
+
+    def close_spider(self, spider):
+        self.exporter.finish_exporting()
+        self.file.close()
