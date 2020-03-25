efba292d7803d0ee3235a0de0552a8c7a796c4f7
sl681 <sam.leigh681@gmail.com>
2020-03-25 17:26:14 +1100

    G20190343010242-Week03

---
+++ Week_03/G20190343010242/task1/web_crawler/web_crawler/pipelines/rrys_pipeline.py
@@ -0,0 +1,23 @@
+import pandas
+import os
+
+
+class RrysPipeline(object):
+
+    def open_spider(self, spider):
+        self.data = []
+
+    def close_spider(self, spider):
+        columns = ['seq', 'title', 'link', 'ranking', 'classification', 'favorites', 'cover']
+        df = pandas.DataFrame(self.data, columns=columns)
+        ROOT_DIR = os.getcwd()
+        path = os.path.join(ROOT_DIR, "output")
+        if not os.path.exists(path):
+            os.makedirs("output")
+        df.to_csv(f"{path}/rrys.csv", columns=columns, index=False,encoding="utf-8")
+
+    def process_item(self, item, spider):
+        if spider.name == 'rrys':
+            self.data.append(dict(item))
+            return item
+        return None
