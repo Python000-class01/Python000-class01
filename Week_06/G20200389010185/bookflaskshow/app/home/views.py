f2668c4f4ea12fed0670315906c6657e7aa45d01
ZaDafu <pptong@foxmail.com>
2020-04-12 18:35:07 +0800

    week 06

---
+++ Week_06/G20200389010185/bookflaskshow/app/home/views.py
@@ -0,0 +1,18 @@
+from flask import render_template
+from . import home
+from app.models import *
+from app import db
+@home.route('/')
+def index():
+        return render_template('/home/index.html')
+
+@home.route('/index')
+def dashboard():
+        return render_template('/home/index.html')
+
+
+@home.route('/result')
+def result():
+        # shorts = T1.query.all()[0:10]
+        shorts = T1.query.order_by(T1.emotions.desc()).all()[0:10]
+        return render_template('/home/result.html', shorts=shorts)
