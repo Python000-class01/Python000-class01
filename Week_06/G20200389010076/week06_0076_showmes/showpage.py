f2188d9c3c41e4846a677d255edeccb07bd9bc9c
ShronLi <1627518968@qq.com>
2020-04-15 20:08:33 +0800

    week06

---
+++ Week_06/G20200389010076/week06_0076_showmes/showpage.py
@@ -0,0 +1,10 @@
+# from flask import Blueprint,render_template
+# from .models import Comment
+#
+# spbp=Blueprint('showpage',__name__)
+#
+#
+# @spbp.route('/')
+# def index():
+#     comments=Comment.query.all()
+#     return render_template('showpage/comment.html',comments=comments)
