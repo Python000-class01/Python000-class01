94f30db7011c40cca08bd302f9d5b33aca2a6b24
Lynda-Li <qiusuo891208@126.com>
2020-04-23 00:40:02 +0800

    结业项目

---
+++ Week_07/G20190389010004/data_process.py
@@ -0,0 +1,45 @@
+import pandas as pd
+import pymysql
+from snownlp import SnowNLP
+
+# 建立mysql数据库连接
+conn = pymysql.connect(host = 'localhost',
+                        port = 3306,
+                        user = 'mysql',
+                        password = 'Pwd_2020',
+                        database = 'testdb',
+                        charset = 'utf8mb4')
+
+cursor = conn.cursor()
+
+comment = pd.read_sql_query('select mid, content, time from sina_comment', conn)
+comment.columns = ['mid', 'content', 'time']
+
+# 数据清理
+comment.info()
+comment.dropna()
+comment.drop_duplicates()
+
+# 情感分析
+def _sentiment(text):
+    return SnowNLP(text).sentiments
+
+comment["sentiment"] = comment.content.apply(_sentiment)
+
+# 处理后存入mysql（新表）
+mid = comment['mid'].values.tolist()
+content = comment['content'].values.tolist()
+sentiment = comment['sentiment'].values.tolist()
+time = comment['time'].values.tolist()
+
+sql = 'insert into sina_comment_sentiment(mid, content, sentiment, time) values (%s, %s, %s, %s)'
+values = [i for i in zip(mid, content, sentiment, time)]
+cursor.executemany(sql, values)
+
+
+# 关闭mysql连接
+cursor.close()
+conn.commit()
+conn.close()
+
+
---
+++ Week_07/G20190389010004/flask/app/models.py
@@ -0,0 +1,14 @@
+# coding: utf-8
+from sqlalchemy import BigInteger, Column, Float, String
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class SinaCommentSentiment(db.Model):
+    __tablename__ = 'sina_comment_sentiment'
+
+    id = db.Column(db.BigInteger, primary_key=True)
+    mid = db.Column(db.String(36), nullable=False, info='评论编号')
+    content = db.Column(db.String(1000), info='评论内容')
+    sentiment = db.Column(db.Float, info='情感評分')
+    time = db.Column(db.String(19), info='评论时间')
