a77d292dfba855a02a8983476f3a02ad545718ce
swortect <swortect@gmail.com>
2020-04-22 21:17:31 +0800

    week7

--- Week_07/G20200389010098/layui/config.py
+++ Week_07/G20200389010098/layui/config.py
@@ -1,9 +1,9 @@
 class Config(object):
     # MySQL Config
-    MYSQL_HOST = 'swortect.mysql.rds.aliyuncs.com'
+    MYSQL_HOST = 'test.mysql.rds.aliyuncs.com'
     MYSQL_PORT = 3306
-    MYSQL_USER = 'swortect'
-    MYSQL_PASSWD = '1q2w3e4r'
+    MYSQL_USER = 'test'
+    MYSQL_PASSWD = 'test'
     MYSQL_DB = 'wordcloud'
     MYSQL_PRE = 'hz_'
     SQLALCHEMY_DATABASE_URI = f'mysql+pymysql://{MYSQL_USER}:{MYSQL_PASSWD}@{MYSQL_HOST}:{MYSQL_PORT}/{MYSQL_DB}?charset=utf8mb4&connect_timeout=10'
